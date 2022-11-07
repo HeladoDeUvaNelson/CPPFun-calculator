188312f21f142648ef765e1a9f36c1e4fbcf4ea8
unknown <nelson227@hotmail.es>
2022-11-07 11:19:10 -0300

    md

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ int main()
 
     do {
         vector<int> operators;
+        string exprOrdered;
+
 
         cout << "Please insert your values\n" << endl;
         getline(cin, expression);
@@ -21,124 +23,168 @@ int main()
         int valuesGot = 0;
         int lastIteration = 0;
         int numOfIterations = 0;
+        int tempValue = -3;
+        bool stopWhile = true;
         result = 0;
 
         if (expression != "exit" && expression != "quit"){
             for (int i = 0; i < expression.length(); i++){
-                if (expression[i] == '+' && i != 0){
+                if (expression[i] == '*'){
+                    string unOrderNums;
                     operators.push_back(1);
-                }else if (expression[i] == '-' && i != 0){
-                    operators.push_back(2);
-                }else if (expression[i] == '*'){
-                    operators.push_back(3);
+                    int j = 1;
+                    while (i-j != -1 && i-j != tempValue && expression[i-j] != '+' && expression[i-j] != '-' && expression[i-j] != '*' && expression[i-j] != '/'){
+                        unOrderNums += expression[i-j];
+                        if (expression[i-j-1] == '+' && i > 0 || expression[i-j-1] == '-'){
+                            unOrderNums += expression[i-j-1];
+                        }
+                        j++;
+                    }
+
+                    for (int i = 1; i <= unOrderNums.length(); i++){
+                        exprOrdered += unOrderNums[unOrderNums.length()-i];
+                    }
+
+                    exprOrdered += expression[i];
+                    j = 1;
+
+                     while (expression[i+j] != '+' && expression[i+j] != '-' && expression[i+j] != '*' && expression[i+j] != '/' && i+j <= expression.length()){
+                        exprOrdered += expression[i+j];
+                        tempValue = i+j;
+                        j++;
+                    }
                 }else if (expression[i] == '/'){
-                    operators.push_back(4);
-                }
-            }
-
-            for (auto oper: operators){
-
-                string numA;
-                string numB;
-
-                int numbersFilled = 0;
-
-                for (int i = 0; i < expression.length(); i++){
-                    if (result == 0 && numOfIterations == 0){
-                        if (numbersFilled == 0){
-                            if (expression[i] != '+' && expression[i] != '-' && expression[i] != '*' && expression[i] != '/'){
-                                numA += expression[i];
-                            }else if (i == 0 && expression[i] == '+' || i == 0 && expression[i] == '-') {
-                                numA += expression[i];
-                            }else {
-                                numbersFilled++;
-                            }
-                    }else if (numbersFilled == 1){
-                            if (expression[i] != '+' && expression[i] != '-' && expression[i] != '*' && expression[i] != '/'){
-                                numB += expression[i];
-                            }else {
-                                numbersFilled++;
-                                lastIteration = i+1;
-                            }
+                    string unOrderNums;
+                    operators.push_back(2);
+                    int j = 1;
+                    while (i-j != -1 && i-j != tempValue && expression[i-j] != '+' && expression[i-j] != '-' && expression[i-j] != '*' && expression[i-j] != '/'){
+                        unOrderNums += expression[i-j];
+                        if (expression[i-j-1] == '+' && i > 0 || expression[i-j-1] == '-'){
+                            unOrderNums += expression[i-j-1];
                         }
-                    }else if (numbersFilled == 0 && i >= lastIteration){
-                        if (expression[i] != '+' && expression[i] != '-' && expression[i] != '*' && expression[i] != '/'){
-                            numB += expression[i];
-                        }else {
-                            numbersFilled++;
-                            lastIteration = i+1;
-                        }
+                        j++;
+                    }
+
+                    for (int i = 1; i <= unOrderNums.length(); i++){
+                        exprOrdered += unOrderNums[unOrderNums.length()-i];
+                    }
+
+                    exprOrdered += expression[i];
+                    j = 1;
+
+                     while (expression[i+j] != '+' && expression[i+j] != '-' && expression[i+j] != '*' && expression[i+j] != '/' && i+j <= expression.length()){
+                        exprOrdered += expression[i+j];
+                        tempValue = i+j;
+                        j++;
                     }
                 }
-
-                cout << numA + " A\n";
-                cout << numB + " B\n";
-
-                switch (oper){
-                    case 1:
-                        if (result == 0){
-                            char c = '-';
-                            size_t found = numA.find(c);
-                            if (found != string::npos){
-                                result = stoi(numA) + stoi(numB);
-                            }else {
-                                result = stoi(numA) + stoi(numB);
-                            }
-                        }else {
-                            result = result + stoi(numB);
-                        }
-
-                        break;
-
-                    case 2:
-                        if (result == 0 && numOfIterations == 0){
-                            char c = '-';
-                            size_t found = numA.find(c);
-                            if (found != string::npos){
-                                result = stoi(numA) - stoi(numB);
-                            }else {
-                                result = stoi(numA) - stoi(numB);
-                            }
-                        }else {
-                            result = result - stoi(numB);
-                        }
-                        break;
-
-                    case 3:
-                        if (result == 0 && numOfIterations == 0){
-                            char c = '-';
-                            size_t found = numA.find(c);
-                            if (found != string::npos){
-                                result = stoi(numA) * stoi(numB);
-                            }else {
-                                result = stoi(numA) * stoi(numB);
-                            }
-                        }else {
-                            result = result - stoi(numB);
-                        }
-                        break;
-
-                    case 4:
-                        if (result == 0 && numOfIterations == 0){
-                            char c = '-';
-                            size_t found = numA.find(c);
-                            if (found != string::npos){
-                                result = stoi(numA) / stoi(numB);
-                            }else {
-                                result = stoi(numA) / stoi(numB);
-                            }
-                        }else {
-                            result = result / stoi(numB);
-                        }
-                        break;
-                }
-
-                numOfIterations++;
             }
+
+            cout << exprOrdered << endl;
+
+//            for (auto oper: operators){
+//
+//                string numA;
+//                string numB;
+//
+//                int numbersFilled = 0;
+//
+//                for (int i = 0; i < expression.length(); i++){
+//                    if (result == 0 && numOfIterations == 0){
+//                        if (numbersFilled == 0){
+//                            if (expression[i] != '+' && expression[i] != '-' && expression[i] != '*' && expression[i] != '/'){
+//                                numA += expression[i];
+//                            }else if (i == 0 && expression[i] == '+' || i == 0 && expression[i] == '-') {
+//                                numA += expression[i];
+//                            }else {
+//                                numbersFilled++;
+//                            }
+//                    }else if (numbersFilled == 1){
+//                            if (expression[i] != '+' && expression[i] != '-' && expression[i] != '*' && expression[i] != '/'){
+//                                numB += expression[i];
+//                            }else {
+//                                numbersFilled++;
+//                                lastIteration = i+1;
+//                            }
+//                        }
+//                    }else if (numbersFilled == 0 && i >= lastIteration){
+//                        if (expression[i] != '+' && expression[i] != '-' && expression[i] != '*' && expression[i] != '/'){
+//                            numB += expression[i];
+//                        }else {
+//                            numbersFilled++;
+//                            lastIteration = i+1;
+//                        }
+//                    }
+//                }
+//
+//                cout << numA + " A\n";
+//                cout << numB + " B\n";
+//
+//                switch (oper){
+//                    case 1:
+//                        if (result == 0){
+//                            char c = '-';
+//                            size_t found = numA.find(c);
+//                            if (found != string::npos){
+//                                result = stoi(numA) + stoi(numB);
+//                            }else {
+//                                result = stoi(numA) + stoi(numB);
+//                            }
+//                        }else {
+//                            result = result + stoi(numB);
+//                        }
+//
+//                        break;
+//
+//                    case 2:
+//                        if (result == 0 && numOfIterations == 0){
+//                            char c = '-';
+//                            size_t found = numA.find(c);
+//                            if (found != string::npos){
+//                                result = stoi(numA) - stoi(numB);
+//                            }else {
+//                                result = stoi(numA) - stoi(numB);
+//                            }
+//                        }else {
+//                            result = result - stoi(numB);
+//                        }
+//                        break;
+//
+//                    case 3:
+//                        if (result == 0 && numOfIterations == 0){
+//                            char c = '-';
+//                            size_t found = numA.find(c);
+//                            if (found != string::npos){
+//                                result = stoi(numA) * stoi(numB);
+//                            }else {
+//                                result = stoi(numA) * stoi(numB);
+//                            }
+//                        }else {
+//                            result = result - stoi(numB);
+//                        }
+//                        break;
+//
+//                    case 4:
+//                        if (result == 0 && numOfIterations == 0){
+//                            char c = '-';
+//                            size_t found = numA.find(c);
+//                            if (found != string::npos){
+//                                result = stoi(numA) / stoi(numB);
+//                            }else {
+//                                result = stoi(numA) / stoi(numB);
+//                            }
+//                        }else {
+//                            result = result / stoi(numB);
+//                        }
+//                        break;
+//                }
+//
+//                numOfIterations++;
+//            }
         }
 
-        cout << " \n";
-        cout << result << + " \n\n";
+//        cout << " \n";
+//        cout << result << + " \n\n";
     }
     while (expression != "exit" && expression != "quit");
 
